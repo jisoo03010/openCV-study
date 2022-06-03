d046e49795fa3a14dad227a67bee8752de405d77
김지수 <040310jisoo@naver.com>
2022-06-03 23:46:23 +0900

    opencv 이미지 풀사이즈로 가져오기

--- SlideShow.py
+++ SlideShow.py
@@ -14,4 +14,20 @@ import cv2
 img_files = glob.glob('.\\images\\*.jpg')
 
 for f in img_files:
-    print(f)
+    print(f) # 이미지 url 가져옴
+
+cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+cv2.setWindowProperty('image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+cnt = len(img_files)
+idx = 0
+while True:
+    img = cv2.imread(img_files[idx])
+    cv2.imshow("image", img)
+    if cv2.waitKey(1000) == 27: #esc click
+        break
+    idx += 1
+    if idx >= cnt:
+        idx = 0
+
+cv2.destroyAllWindows()
